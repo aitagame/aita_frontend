de2cef0c3f775875d8a5d93b16b487b91c5900ad
Olya Chamlai <vacyakaktyc@gmail.com>
2022-02-11 23:54:48 +0300

    AITA-4_add prettier and linter

--- src/game/styled.ts
+++ src/game/styled.ts
@@ -1,6 +1,6 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const Canvas = styled.canvas`
-	display: block;
-	border: 1px solid black;
-`;
+  display: block;
+  border: 1px solid black;
+`
