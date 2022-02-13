e8af4141a2bf6f852c8dffd1c20715542e30cd8d
cybor97 <muhametshin.1997@gmail.com>
2022-02-13 17:45:13 +0200

    AITA-7: Fix esling warnings (for CI build)

--- src/game/utils/loaderImages.ts
+++ src/game/utils/loaderImages.ts
@@ -1,14 +1,16 @@
 export default function loaderImages(
   load: HTMLImageElement[],
-  callback: (arg: any) => void,
-  ...params: any
+  /* eslint-disable  @typescript-eslint/no-explicit-any */
+  callback: (...args: any[]) => void,
+  /* eslint-disable  @typescript-eslint/no-explicit-any */
+  ...params: any[]
 ) {
   let counter = 0
   console.dir(load)
   function updateConuter() {
     counter++
     if (counter == load.length) {
-      callback(params)
+      callback(...params)
     }
   }
   load.forEach((el: HTMLImageElement) => {
