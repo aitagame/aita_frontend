9fbd3a6646efeacaea3a73d79900ad6a74ab1711
Olya Chamlai <vacyakaktyc@gmail.com>
2022-02-16 19:23:45 +0300

    AITA-18 move images to assets

--- src/views/data/main.ts
+++ src/views/data/main.ts
@@ -1,4 +1,7 @@
 import { SectionItem } from 'views/types/mainPage'
+import mainInfoImage from 'views/assets/mainInfo.jpg'
+import characterImage from 'views/assets/character.jpg'
+import cristalImage from 'views/assets/cristal.jpg'
 
 export const mainBackground =
   'https://store-images.s-microsoft.com/image/apps.59352.14333371177529780.b6b0e65b-5f45-4caa-81d5-0a50416a56ba.cfeee9be-c402-4365-9ed4-f3d18bdc14a4?mode=scale&q=90&h=1080&w=1920'
@@ -12,13 +15,13 @@ export const mainSectionsData: SectionItem[] = [
     id: 'ksjdla',
     title: 'Section 1 title',
     text: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
-    url: 'https://cms-assets.tutsplus.com/cdn-cgi/image/width=850/uploads/users/723/posts/32139/final_image/ghost-final.jpg',
+    url: mainInfoImage,
   },
   {
     id: 'gdfgd',
     title: 'Section 2 title',
     text: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
-    url: 'https://cms-assets.tutsplus.com/cdn-cgi/image/width=850/uploads/users/723/posts/32139/final_image/ghost-final.jpg',
+    url: mainInfoImage,
   },
 ]
 
@@ -32,25 +35,25 @@ export const infoSectionData: SectionItem[] = [
     id: 'ksjdla',
     title: 'Section 1 title',
     text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
-    url: 'https://qph.fs.quoracdn.net/main-qimg-7fd43f265a51896116b7aa04344620eb.webp',
+    url: characterImage,
   },
   {
     id: 'gdfgd',
     title: 'Section 2 title',
     text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
-    url: 'https://qph.fs.quoracdn.net/main-qimg-7fd43f265a51896116b7aa04344620eb.webp',
+    url: characterImage,
   },
   {
     id: 'gddfsfgd',
     title: 'Section 3 title',
     text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
-    url: 'https://qph.fs.quoracdn.net/main-qimg-7fd43f265a51896116b7aa04344620eb.webp',
+    url: characterImage,
   },
   {
     id: 'gddfsfdfsgd',
     title: 'Section 4 title',
     text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
-    url: 'https://qph.fs.quoracdn.net/main-qimg-7fd43f265a51896116b7aa04344620eb.webp',
+    url: characterImage,
   },
 ]
 
@@ -63,42 +66,42 @@ export const itemsSectionData: SectionItem[] = [
     id: 'ksjdla',
     title: 'Section 1 title',
     text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
-    url: 'https://c.neh.tw/thumb/f/350/5146363029356544.jpg',
+    url: cristalImage,
   },
   {
     id: 'gdfgd',
     title: 'Section 2 title',
     text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
-    url: 'https://c.neh.tw/thumb/f/350/5146363029356544.jpg',
+    url: cristalImage,
   },
   {
     id: 'gddfsfgd',
     title: 'Section 3 title',
     text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
-    url: 'https://c.neh.tw/thumb/f/350/5146363029356544.jpg',
+    url: cristalImage,
   },
   {
     id: 'gddfsfdfsgd',
     title: 'Section 4 title',
     text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
-    url: 'https://c.neh.tw/thumb/f/350/5146363029356544.jpg',
+    url: cristalImage,
   },
   {
     id: 'gddfsfваыdfsgd',
     title: 'Section 5 title',
     text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
-    url: 'https://c.neh.tw/thumb/f/350/5146363029356544.jpg',
+    url: cristalImage,
   },
   {
     id: 'gddfsfsdfыdfsgd',
     title: 'Section 6 title',
     text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
-    url: 'https://c.neh.tw/thumb/f/350/5146363029356544.jpg',
+    url: cristalImage,
   },
   {
     id: 'gddfsfваdfsfsgd',
     title: 'Section 7 title',
     text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
-    url: 'https://c.neh.tw/thumb/f/350/5146363029356544.jpg',
+    url: cristalImage,
   },
 ]
--- src/views/main/index.tsx
+++ src/views/main/index.tsx
@@ -1,17 +1,18 @@
 import { PromoTitle, PromoWrapper, MainWrapper, PromoContent } from './styled'
 import { Header } from 'views/components/Header'
 import { MainSection } from 'views/components/MainSection'
-import { mainBackground, mainSectionsData } from 'views/data/main'
+import { mainSectionsData } from 'views/data/main'
 import { InfoSection } from 'views/components/InfoSection'
 import { ItemsSection } from 'views/components/ItemsSection'
 import { Footer } from 'views/components/Footer'
 import { PlayGameButton } from 'views/components/PlayGameButton'
+import promoImage from 'views/assets/promo.jpg'
 
 export const Main: React.FC = () => {
   return (
     <MainWrapper>
       <Header />
-      <PromoWrapper background={mainBackground}>
+      <PromoWrapper background={promoImage}>
         <PromoContent>
           <PromoTitle>Main Promo Title</PromoTitle>
           <PlayGameButton title="Join Today" />
