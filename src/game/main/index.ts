3021afeee3a4842da3ea73ed4041094d400dc260
TangerineNe <rusy301997@mail.ru>
2022-02-20 05:42:42 +0300

    AITA-12 dinamic background

--- src/game/main/index.ts
+++ src/game/main/index.ts
@@ -1,5 +1,6 @@
 import loaderImages from '../utils/loaderImages';
 import { IGameData } from '../gameData';
+
 class Pointer {
   x: number;
   y: number;
@@ -26,11 +27,28 @@ class Background {
     else this.center = new Pointer(width / 2, height / 2);
   }
   render(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
+    let sizeFactor = 1;
+    if (this.width / this.height < canvas.width / canvas.height) {
+      sizeFactor = canvas.width / this.width;
+    } else {
+      sizeFactor = canvas.height / this.height;
+    }
+    let dxCenter = this.center.x - this.width / 2;
+    let dyCenter = this.center.y - this.height / 2;
+    const sizeEdgeW = (this.width * sizeFactor) / 2 - canvas.width / 2;
+    const sizeEdgeH = (this.height * sizeFactor) / 2 - canvas.height / 2;
+    dxCenter = Math.min(Math.abs(dxCenter), sizeEdgeW) * Math.sign(dxCenter);
+    dyCenter = Math.min(Math.abs(dyCenter), sizeEdgeH) * Math.sign(dyCenter);
+
     ctx.save();
-    const dx = this.center.x - canvas.width / 2;
-    const dy = this.center.y - canvas.height / 2;
-    ctx.translate(canvas.width / 2, canvas.height / 2);
-    ctx.drawImage(this.img, -this.width / 2, -this.height / 2, this.width, this.height);
+    ctx.translate(canvas.width / 2 - dxCenter, canvas.height / 2 - dyCenter);
+    ctx.drawImage(
+      this.img,
+      (-this.width * sizeFactor) / 2,
+      (-this.height * sizeFactor) / 2,
+      this.width * sizeFactor,
+      this.height * sizeFactor
+    );
     ctx.restore();
   }
 }
@@ -46,12 +64,13 @@ export class Game {
   players: Player[];
   lastFrameUpdate: number;
 
-  constructor(gameData: IGameData) {
-    this.background = new Background(1600, 844, gameData.backgroundImage);
+  constructor(gameData: IGameData, canvas: HTMLCanvasElement) {
+    this.background = new Background(1600, 844, gameData.backgroundImage, new Pointer(980, 400));
     this.players = [];
     this.lastFrameUpdate = Date.now();
     window.addEventListener('resize', () => {
-      console.log('handeResize');
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
     });
   }
 
