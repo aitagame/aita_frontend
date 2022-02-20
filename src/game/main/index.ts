c0e2c87de288cc3db6c5565671c1b395ca96f36d
TangerineNe <rusy301997@mail.ru>
2022-02-20 04:06:16 +0300

    AITA-12 return semicolons

--- src/game/main/index.ts
+++ src/game/main/index.ts
@@ -1,75 +1,81 @@
-import loaderImages from '../utils/loaderImages'
-import { IGameData } from '../gameData'
+import loaderImages from '../utils/loaderImages';
+import { IGameData } from '../gameData';
 class Pointer {
-  x: number
-  y: number
+  x: number;
+  y: number;
   constructor(x: number, y: number) {
-    this.x = x
-    this.y = y
+    this.x = x;
+    this.y = y;
   }
   distance(ptr: Pointer): number {
-    return Math.hypot(Math.abs(this.x - ptr.x), Math.abs(this.y - ptr.y))
+    return Math.hypot(Math.abs(this.x - ptr.x), Math.abs(this.y - ptr.y));
   }
 }
 
 class Background {
-  width: number
-  height: number
-  center: Pointer
-  img: HTMLImageElement
+  width: number;
+  height: number;
+  center: Pointer;
+  img: HTMLImageElement;
   constructor(width: number, height: number, src: string, center?: Pointer) {
-    this.width = width
-    this.height = height
-    this.img = new Image(width, height)
-    this.img.src = src
-    if (center) this.center = center
-    else this.center = new Pointer(width / 2, height / 2)
+    this.width = width;
+    this.height = height;
+    this.img = new Image(width, height);
+    this.img.src = src;
+    if (center) this.center = center;
+    else this.center = new Pointer(width / 2, height / 2);
   }
   render(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
-    ctx.drawImage(this.img, 0, 0, canvas.width, canvas.height)
+    ctx.save();
+    const dx = this.center.x - canvas.width / 2;
+    const dy = this.center.y - canvas.height / 2;
+    ctx.translate(canvas.width / 2, canvas.height / 2);
+    ctx.drawImage(this.img, -this.width / 2, -this.height / 2, this.width, this.height);
+    ctx.restore();
   }
 }
 
 class Player {
-  cords: Pointer
+  cords: Pointer;
   constructor(cords: Pointer) {
-    this.cords = cords
+    this.cords = cords;
   }
 }
 export class Game {
-  background: Background
-  players: Player[]
-  lastFrameUpdate: number
+  background: Background;
+  players: Player[];
+  lastFrameUpdate: number;
 
   constructor(gameData: IGameData) {
-    this.background = new Background(800, 600, gameData.backgroundImage)
-    this.players = []
-    this.lastFrameUpdate = Date.now()
+    this.background = new Background(1600, 844, gameData.backgroundImage);
+    this.players = [];
+    this.lastFrameUpdate = Date.now();
     window.addEventListener('resize', () => {
-      console.log('handeResize')
-    })
+      console.log('handeResize');
+    });
   }
 
   play(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
-    const now = Date.now()
-    const dt = (now - this.lastFrameUpdate) / 1000
-    this.update(dt)
-    this.render(ctx, canvas)
-    requestAnimationFrame(() => this.play(ctx, canvas))
-    this.lastFrameUpdate = now
+    const now = Date.now();
+    const dt = (now - this.lastFrameUpdate) / 1000;
+    this.update(dt);
+    this.render(ctx, canvas);
+    requestAnimationFrame(() => this.play(ctx, canvas));
+    this.lastFrameUpdate = now;
   }
 
   update(dt: number) {
-    console.log(dt)
+    //console.log(dt)
   }
 
   render(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
-    this.background.render(ctx, canvas)
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    this.background.render(ctx, canvas);
   }
 
   startGame(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight
-    loaderImages([this.background.img], this.play.bind(this), ctx, canvas)
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    loaderImages([this.background.img], this.play.bind(this), ctx, canvas);
   }
 }
