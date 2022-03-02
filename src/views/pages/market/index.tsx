13c119a4ee896237321969bd7615798db2ee14bf
Rio Cantre <smittenaf301icu@gmail.com>
2022-03-02 18:51:33 +0100

    AITA-45 fix app file

--- src/views/pages/market/index.tsx
+++ src/views/pages/market/index.tsx
@@ -1,5 +1,12 @@
+import { ContentWrapper, TitleMarket } from './styled';
 import { BaseLayout } from 'views/components/BaseLayout';
 
 export const Market = () => {
-  return <BaseLayout></BaseLayout>;
+  return (
+    <BaseLayout>
+      <ContentWrapper>
+        <TitleMarket>Coming Soon</TitleMarket>
+      </ContentWrapper>
+    </BaseLayout>
+  );
 };
--- src/views/pages/market/styled.ts
+++ src/views/pages/market/styled.ts
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
+import { TitleH1 } from 'views/components/Title';
 
-export const Wrapper = styled.div`
-  background-color: black;
-  color: yellow;
+export const ContentWrapper = styled.div`
+  height: 77vh;
+`;
+
+export const TitleMarket = styled(TitleH1)`
+  margin-top: 15rem;
+  display: flex;
+  justify-content: center;
 `;
