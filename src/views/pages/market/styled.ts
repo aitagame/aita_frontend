3a5d70fee081746c8c384d496ac7b94f94371f8e
Rio Cantre <smittenaf301icu@gmail.com>
2022-03-04 20:20:13 +0100

    AITA-8 updated market style

--- src/views/pages/market/styled.ts
+++ src/views/pages/market/styled.ts
@@ -3,10 +3,11 @@ import { TitleH1 } from 'views/components/Title';
 import { Wrapper } from 'views/components/Wrapper';
 
 export const ContentWrapper = styled(Wrapper)`
-  height: 77vh;
+  height: 67vh;
   margin: auto;
 `;
 
 export const TitleMarket = styled(TitleH1)`
-  margin-top: 15rem;
+  margin-top: 11rem;
+  text-align: center;
 `;
