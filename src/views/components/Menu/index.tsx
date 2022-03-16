6d57b5e6e4708503dbd30882691fe50356778c6f
Olya Chamlai <vacyakaktyc@gmail.com>
2022-03-17 01:39:36 +0300

    AITA-55 set active link for header

--- src/views/components/Menu/index.tsx
+++ src/views/components/Menu/index.tsx
@@ -1,17 +1,42 @@
 import { useState } from 'react';
+import { useLocation } from 'react-router';
 import { BurgerMenuProps } from 'views/types/menu';
 import { AppLink } from '../AppLink';
 import { Burger, Menu, Separator } from './styled';
 import { UserMenuList } from './UserMenu';
 
+const menuLinks = {
+  profile: {
+    path: '/profile',
+    title: 'Profile',
+  },
+  market: {
+    path: '/market',
+    title: 'Market',
+  },
+  battle: {
+    path: '/battle-select',
+    title: 'Battle',
+  },
+  game: {
+    path: '/play',
+    title: 'Play',
+  },
+  near: {
+    path: '/hackathon',
+    title: 'NEAR Challenges',
+  },
+};
+
 export const MenuList: React.FC = () => {
+  const location = useLocation();
   return (
     <>
-      <AppLink to="/profile">Profile</AppLink>
-      <AppLink to="/market">Market</AppLink>
-      <AppLink to="/battle-select">Battle</AppLink>
-      <AppLink to="/play">Play</AppLink>
-      <AppLink to="/hackathon">NEAR Challenges</AppLink>
+      {Object.values(menuLinks).map(link => (
+        <AppLink key={link.path} to={link.path} active={location.pathname === link.path}>
+          {link.title}
+        </AppLink>
+      ))}
     </>
   );
 };
