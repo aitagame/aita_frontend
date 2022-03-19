917cf87453e64e0a76407a6d1cd05945969cd4b0
Rio Cantre <smittenaf301icu@gmail.com>
2022-03-19 11:55:35 +0100

    AITA-57 added crystal descriptions

--- src/views/pages/main/data.ts
+++ src/views/pages/main/data.ts
@@ -2,6 +2,13 @@ import { SectionItem } from 'views/types/mainPage';
 import mainInfoImage from 'views/assets/mainInfo.jpg';
 import characterImage from 'views/assets/character.jpg';
 import cristalImage from 'views/assets/cristal.jpg';
+import airCrystal from 'views/assets/airCrystal.png';
+import darkCrystal from 'views/assets/darkCrystal.png';
+import deathCrystal from 'views/assets/deathCrystal.png';
+import earthCrystal from 'views/assets/earthCrystal.png';
+import fireCrystal from 'views/assets/fireCrystal.png';
+import lifeCrystal from 'views/assets/lifeCrystal.png';
+import waterCrystal from 'views/assets/waterCrystal.png';
 
 export const mainBackground =
   'https://store-images.s-microsoft.com/image/apps.59352.14333371177529780.b6b0e65b-5f45-4caa-81d5-0a50416a56ba.cfeee9be-c402-4365-9ed4-f3d18bdc14a4?mode=scale&q=90&h=1080&w=1920';
@@ -63,45 +70,45 @@ export const SectionTitle = 'Where does it come from?';
 
 export const itemsSectionData: SectionItem[] = [
   {
-    id: 'ksjdla',
-    title: 'Section 1 title',
-    text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
-    url: cristalImage,
+    id: 'dark crystal',
+    title: 'Dark Crystal',
+    text: 'The powerful crystal which can be obtain with 20 life crystals',
+    url: darkCrystal,
   },
   {
-    id: 'gdfgd',
-    title: 'Section 2 title',
-    text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
-    url: cristalImage,
+    id: 'life crystal',
+    title: 'Life Crystal',
+    text: 'Gain eternal life by collecting all 4 elemental crystals',
+    url: lifeCrystal,
   },
   {
-    id: 'gddfsfgd',
-    title: 'Section 3 title',
-    text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
-    url: cristalImage,
+    id: 'death crystal',
+    title: 'Death Crystal',
+    text: 'The crystal of death',
+    url: deathCrystal,
   },
   {
-    id: 'gddfsfdfsgd',
-    title: 'Section 4 title',
-    text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
-    url: cristalImage,
+    id: 'water crystal',
+    title: 'Water Crystal',
+    text: 'The elemental crystal of water',
+    url: waterCrystal,
   },
   {
-    id: 'gddfsfваыdfsgd',
-    title: 'Section 5 title',
-    text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
-    url: cristalImage,
+    id: 'fire crystal',
+    title: 'Fire Crystal',
+    text: 'The elemental crystal of fire',
+    url: fireCrystal,
   },
   {
-    id: 'gddfsfsdfыdfsgd',
-    title: 'Section 6 title',
-    text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
-    url: cristalImage,
+    id: 'earth crystal',
+    title: 'Earth Crystal',
+    text: 'The elemental crystal of earth',
+    url: earthCrystal,
   },
   {
-    id: 'gddfsfваdfsfsgd',
-    title: 'Section 7 title',
-    text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal',
-    url: cristalImage,
+    id: 'air crystal',
+    title: 'Air Crystal',
+    text: 'The elemental crystal of air',
+    url: airCrystal,
   },
 ];
