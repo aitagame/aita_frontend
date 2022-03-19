86978244eee73d3ef57c63071acc56f261ab4174
Olya Chamlai <vacyakaktyc@gmail.com>
2022-03-19 21:16:33 +0300

    AITA-58 disable any type error

--- src/views/hooks/useNear.ts
+++ src/views/hooks/useNear.ts
@@ -51,6 +51,7 @@ const signOut = () => {
 };
 
 const getAccountBalance = (accountId: string) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return (contract as any).ft_balance_of({ account_id: accountId });
 };
 
